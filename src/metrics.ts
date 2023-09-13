4417fbe5c3ca73203047c2b6e21f661675fa0ce9
Primexz <johann@pepebot.info>
2023-09-13 23:41:43 +0200

    simplify algorithm to only one reducer

--- src/metrics.ts
+++ src/metrics.ts
@@ -11,10 +11,6 @@ interface IRawVPNHashMap {
     [userId: string]: IVPNStat[]
 }
 
-interface IVPNHashMap {
-    [userId: string]: IVPNStat
-}
-
 export class Metrics {
     private app: Express
 
@@ -29,42 +25,27 @@ export class Metrics {
 
         return vpns.reduce((hash, cur) => {
             if (!hash[cur.userId]) {
-                hash[cur.userId] = []
+                hash[cur.userId] = {
+                    rx: 0,
+                    tx: 0,
+                }
             }
 
             const vpn = stats.find((statVpn) => statVpn.publicKey === cur.pub)
             if (vpn?.transferRx != undefined && vpn?.transferTx != undefined) {
-                hash[cur.userId].push({
-                    rx: vpn.transferRx,
-                    tx: vpn.transferTx,
-                })
+                hash[cur.userId].rx += vpn.transferRx
+                hash[cur.userId].tx += vpn.transferTx
             }
 
             return hash
         }, {})
     }
 
-    private combineHashMapVpns(hashmap: IRawVPNHashMap) {
-        return Object.keys(hashmap).reduce((outMap, key) => {
-            outMap[key] = hashmap[key].reduce(
-                (acc, vpn) => {
-                    acc.tx += vpn.tx
-                    acc.rx += vpn.rx
-                    return acc
-                },
-                { rx: 0, tx: 0 },
-            )
-
-            return outMap
-        }, {} as IVPNHashMap)
-    }
-
     private registerRoutes() {
         this.app.get("/metrics", async (req, res, next) => {
-            const rawHashMap = await this.getUserVpnHashMap()
-            const combinedHashMap = this.combineHashMapVpns(rawHashMap)
+            const hashmap = await this.getUserVpnHashMap()
 
-            console.log(combinedHashMap)
+            console.log(hashmap)
 
             res.send("metrics works")
         })
