9fb4e7b390e5951f81fd6adbe6ffa91fb23a30dc
Primexz <johann@pepebot.info>
2023-10-01 18:54:28 +0200

    feat: add cluster support

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,35 @@
-import RedisStore from "connect-redis"
-import express from "express"
-import expressSession from "express-session"
-import morgan from "morgan"
-import { config } from "./config"
-import { InternalMetrics } from "./internal-metrics"
-import { Metrics } from "./metrics"
-import { Redis } from "./modules/Redis"
-import { NodeFactory } from "./modules/nodes/node-factory"
-import { ConfigManager } from "./modules/server/config-manager"
-import mainRouter from "./routes/index"
-import { JSONSchemaValidator } from "./schema-validator"
+import cluster from "node:cluster"
+import { cpus } from "node:os"
 import * as util from "./util"
+
+function createForks() {
+    const cpuCnt = cpus().length
+    for (let i = 0; i < cpuCnt; i++) {
+        cluster.fork()
+    }
+}
+
 ;(async () => {
-    console.log("Starting Radical VPN Backend Server..")
+    if (cluster.isPrimary) {
+        console.log("Starting Radical VPN Backend Server - Cluster Primary")
 
-    console.log("Starting database migration")
-    await util.exec("npx knex migrate:latest --env production")
+        console.log("Starting database migration")
+        console.log(await util.exec("npx knex migrate:latest --env production"))
 
-    try {
-        const wgVersion = (await util.exec("wg --version")).split(" ")[1]
-        console.log(`Found wireguard installation -> ${wgVersion}`)
-    } catch (err) {
-        console.log("No valid wireguard installation found. Exiting..")
-        process.exit(1)
-    }
+        createForks()
 
-    const app = express()
-    const redis = await Redis.getInstance()
+        cluster.on("exit", (worker, code, signal) => {
+            const infos = { code, signal }
 
-    // Initialize store.
-    let redisStore = new RedisStore({
-        client: redis,
-        prefix: "radical_vpn:session:",
-    })
-
-    //register middlewares
-    let sessionConfig: expressSession.SessionOptions = {
-        store: redisStore,
-        secret: config.SERVER.SESSION_SECRET,
-        name: "RADICAL_SESSION_ID",
-        resave: true,
-        saveUninitialized: true,
-        cookie: {
-            secure: false,
-        },
-    }
-    if (app.get("env") === "production") {
-        app.set("trust proxy", 1)
-
-        // @ts-ignore
-        sessionConfig.cookie.secure = true
-    }
-
-    app.use(expressSession(sessionConfig))
-    app.use(express.json())
-    app.use(morgan("dev"))
-
-    //register main router
-    app.use(mainRouter)
-
-    await ConfigManager.publishServerConfig("all")
-
-    const nodes = await new NodeFactory().getAll()
-    await Promise.all(
-        nodes.map(async (_node) => {
-            const hostname = _node.hostname
-            await redis.publish(`start_interface:${hostname}`, "")
-            console.log(`starting vpn node ${hostname}`)
-        }),
-    )
-
-    await JSONSchemaValidator.setup()
-
-    app.listen(config.SERVER.HTTP_PORT, () => {
+            console.log(
+                `worker ${worker.process.pid} died ${infos}, respawning..`,
+            )
+            cluster.fork()
+        })
+    } else {
         console.log(
-            `Started Radical VPN Backend Server on 127.0.0.1:${config.SERVER.HTTP_PORT}`,
+            `Starting Radical VPN Backend Server - Cluster Worker ${process.pid}`,
         )
-    })
-
-    console.log("Starting Metrics server")
-    const metrics = new Metrics()
-    metrics.start()
-
-    console.log("Starting Internal Metrics server")
-    const internalMetrics = new InternalMetrics()
-    internalMetrics.start()
+        require("./server")
+    }
 })()
---
+++ src/server.ts
@@ -0,0 +1,85 @@
+import RedisStore from "connect-redis"
+import express from "express"
+import expressSession from "express-session"
+import morgan from "morgan"
+import { config } from "./config"
+import { InternalMetrics } from "./internal-metrics"
+import { Metrics } from "./metrics"
+import { Redis } from "./modules/Redis"
+import { NodeFactory } from "./modules/nodes/node-factory"
+import { ConfigManager } from "./modules/server/config-manager"
+import mainRouter from "./routes/index"
+import { JSONSchemaValidator } from "./schema-validator"
+import * as util from "./util"
+;(async () => {
+    console.log("starting radical vpn backend server")
+
+    try {
+        const wgVersion = (await util.exec("wg --version")).split(" ")[1]
+        console.log(`Found wireguard installation -> ${wgVersion}`)
+    } catch (err) {
+        console.log("No valid wireguard installation found. Exiting..")
+        process.exit(1)
+    }
+
+    const app = express()
+    const redis = await Redis.getInstance()
+
+    // Initialize store.
+    let redisStore = new RedisStore({
+        client: redis,
+        prefix: "radical_vpn:session:",
+    })
+
+    //register middlewares
+    let sessionConfig: expressSession.SessionOptions = {
+        store: redisStore,
+        secret: config.SERVER.SESSION_SECRET,
+        name: "RADICAL_SESSION_ID",
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            secure: false,
+        },
+    }
+    if (app.get("env") === "production") {
+        app.set("trust proxy", 1)
+
+        // @ts-ignore
+        sessionConfig.cookie.secure = true
+    }
+
+    app.use(expressSession(sessionConfig))
+    app.use(express.json())
+    app.use(morgan("dev"))
+
+    //register main router
+    app.use(mainRouter)
+
+    await ConfigManager.publishServerConfig("all")
+
+    const nodes = await new NodeFactory().getAll()
+    await Promise.all(
+        nodes.map(async (_node) => {
+            const hostname = _node.hostname
+            await redis.publish(`start_interface:${hostname}`, "")
+            console.log(`starting vpn node ${hostname}`)
+        }),
+    )
+
+    await JSONSchemaValidator.setup()
+
+    app.listen(config.SERVER.HTTP_PORT, () => {
+        console.log(
+            `Started Radical VPN Backend Server on 127.0.0.1:${config.SERVER.HTTP_PORT}`,
+        )
+    })
+
+    console.log("Starting Metrics server")
+    const metrics = new Metrics()
+    metrics.start()
+
+    console.log("Starting Internal Metrics server")
+    const internalMetrics = new InternalMetrics()
+    internalMetrics.start()
+})()
