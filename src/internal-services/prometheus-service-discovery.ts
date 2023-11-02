9195ba382238950e235522d80be4e5d75489d49b
Primexz <johann@pepebot.info>
2023-11-02 22:23:56 +0100

    fix(sd): use correct prop

--- src/internal-services/prometheus-service-discovery.ts
+++ src/internal-services/prometheus-service-discovery.ts
@@ -30,7 +30,7 @@ export class PrometheusServiceDiscovery extends GenericInternalService {
                     (exporter) => `${node.data.internal_ip}:${exporter}`,
                 ),
                 labels: {
-                    __meta_dc: data.country,
+                    __meta_dc: data.country_name,
                     __meta_city: data.city,
                     __meta_hostname: data.hostname,
                     __meta_externalIp: data.external_ip,
