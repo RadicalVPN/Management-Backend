7d13f755b999e0f45a41afd66673adef31734369
Primexz <johann@pepebot.info>
2023-12-05 23:03:52 +0100

    fix(migration): use correct table o-o

--- migrations/20231205220221_add_external_ipv6_to_nodes.ts
+++ migrations/20231205220221_add_external_ipv6_to_nodes.ts
@@ -1,7 +1,7 @@
 import { Knex } from "knex"
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.table("vpns", (table) => {
+    await knex.schema.table("nodes", (table) => {
         table.boolean("external_ip6").defaultTo(false)
     })
 }
