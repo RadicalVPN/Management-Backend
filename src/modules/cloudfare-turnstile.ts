0601b5a699f306d2f98e0267ecbbba139e9ae5b1
Primexz <johann@pepebot.info>
2023-12-07 01:35:36 +0100

    feat: add cloudflare turnstile implementation

---
+++ src/modules/cloudfare-turnstile.ts
@@ -0,0 +1,44 @@
+import axios from "axios"
+import querystring from "querystring"
+import { config } from "../config"
+
+interface IChallengeData {
+    success: boolean
+    "error-codes": string[]
+    challenge_ts?: string
+    hostname?: string
+    action?: string
+    cdata?: string
+    metadata?: {
+        interactive?: boolean
+    }
+}
+
+export class CloudflareTurnstile {
+    constructor(readonly challengeId: string) {}
+
+    async verify(): Promise<boolean> {
+        const challengeData = await this.fetchChallengeData()
+        return challengeData.success
+    }
+
+    private async fetchChallengeData(): Promise<IChallengeData> {
+        try {
+            return (
+                await axios.post(
+                    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+                    querystring.stringify({
+                        secret: config.ClOUDFLARE.TURNSTILE.SECRET_KEY,
+                        response: this.challengeId,
+                    }),
+                )
+            ).data
+        } catch (e) {
+            console.error(e)
+            return {
+                success: false,
+                "error-codes": ["internal-server-error-radical"],
+            }
+        }
+    }
+}
