e0e784a3b86504784d45314beaab6a7529ef972f
Primexz <johann@pepebot.info>
2023-12-10 21:49:23 +0100

    feat(db): add more oauth2 tables

---
+++ migrations/20231210203624_create_oauth2_authorization_codes.ts
@@ -0,0 +1,27 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema
+        .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+        .createTableIfNotExists("oauth_authorization_codes", (table) => {
+            table
+                .uuid("id", { primaryKey: true, useBinaryUuid: true })
+                .defaultTo(knex.raw("uuid_generate_v4()"))
+
+            table.string("authorizationCode")
+            table.date("expiresAt")
+            table.string("redirectUri")
+            table.string("scope")
+            table.uuid("userId")
+            table.string("clientId")
+
+            table.foreign("userId").references("users.id")
+            table.foreign("clientId").references("oauth_clients.clientId")
+
+            table.timestamps(true, true, true)
+        })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("oauth_authorization_codes")
+}
---
+++ migrations/20231210204602_oauth2_access_tokens.ts
@@ -0,0 +1,29 @@
+import { Knex } from "knex"
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema
+        .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+        .createTableIfNotExists("oauth_access_tokens", (table) => {
+            table
+                .uuid("id", { primaryKey: true, useBinaryUuid: true })
+                .defaultTo(knex.raw("uuid_generate_v4()"))
+
+            table.string("accessToken")
+            table.date("accessTokenExpiresAt")
+
+            table.string("refreshToken")
+            table.date("refreshTokenExpiresAt")
+
+            table.uuid("userId")
+            table.string("clientId")
+
+            table.foreign("userId").references("users.id")
+            table.foreign("clientId").references("oauth_clients.clientId")
+
+            table.timestamps(true, true, true)
+        })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("oauth_access_tokens")
+}
