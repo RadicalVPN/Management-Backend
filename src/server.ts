45eda93cd9caa23e514c22933094266102b563d0
Primexz <johann@pepebot.info>
2023-12-25 21:55:53 +0100

    fix: add 404 handler

--- src/server.ts
+++ src/server.ts
@@ -82,6 +82,12 @@ const app = express()
     //register main router
     app.use(mainRouter)
 
+    app.use((req, res, next) => {
+        res.status(404).json({
+            error: "Not Found",
+        })
+    })
+
     await ConfigManager.publishServerConfig("all")
 
     const nodes = await new NodeFactory().getAll()
